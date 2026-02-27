c4d2a4d4785a577b398d1e42bda96bd2db88417c
Senyang Su <susenyang@163.com>
2026-02-28 02:03:53 +0800

    规范化 sublime 中的 tab

--- code/math/QuickPower.cpp
+++ code/math/QuickPower.cpp
@@ -3,24 +3,24 @@
 using i64 = long long;
 
 i64 qpow(i64 a, i64 b, int mod) {
-    i64 res = 1;
-    while (b) {
-        if (b & 1) {
-            res = res * a % mod;
-        }
-        a = a * a % mod;
-        b >>= 1;
-    }
-    return res;
+	i64 res = 1;
+	while (b) {
+		if (b & 1) {
+			res = res * a % mod;
+		}
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return res;
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-    // example
-    i64 a = 2, b = 10, mod = 1e9 + 7;
-    std::cout << qpow(a, b, mod) << '\n';
+	// example
+	i64 a = 2, b = 10, mod = 1e9 + 7;
+	std::cout << qpow(a, b, mod) << '\n';
 
-    return 0;
+	return 0;
 }
